a5a58255884bc8ca7078872b6c53015431bc707d
caina <cainacc@hotmail.com>
2025-06-07 10:42:10 -0300

    open links, round borders, optimize images

--- Assets/Scripts/GameElement.cs
+++ Assets/Scripts/GameElement.cs
@@ -1,11 +1,28 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class GameElement : MonoBehaviour
 {
+    [SerializeField]
+    private ScrollRect scrollRect = null;
     [SerializeField]
     private string url = null;
     public void OnPointerDown()
     {
         Application.OpenURL(url);
     }
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        ExecuteEvents.Execute(scrollRect.gameObject, eventData, ExecuteEvents.beginDragHandler);
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+        ExecuteEvents.Execute(scrollRect.gameObject, eventData, ExecuteEvents.dragHandler);
+    }
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        ExecuteEvents.Execute(scrollRect.gameObject, eventData, ExecuteEvents.endDragHandler);
+    }
+
 }

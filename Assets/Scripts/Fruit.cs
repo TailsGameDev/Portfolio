33c50290d028d28bedbbc04ff4a584f58873aa9c
caina <cainacc@hotmail.com>
2025-06-10 13:44:28 -0300

    adjusted delta to fit min and max bounds for cut force

--- Assets/Scripts/Fruit.cs
+++ Assets/Scripts/Fruit.cs
@@ -7,6 +7,10 @@ public class Fruit : MonoBehaviour, IPointerEnterHandler
     private float speed = 0.0f;
     [SerializeField]
     private float forceMultiplier = 0.0f;
+    [SerializeField]
+    private float maxForce = 0.0f;
+    [SerializeField]
+    private float minForce = 0.0f;
 
     [SerializeField]
     private FruitPiece fruitPiece1 = null;
@@ -40,10 +44,40 @@ public class Fruit : MonoBehaviour, IPointerEnterHandler
         FruitPiece piece = Instantiate(fruitPiece1, transform.position, transform.rotation, parent: transform.parent);
         FruitPiece piece2 = Instantiate(fruitPiece2, transform.position, transform.rotation, parent: transform.parent);
 
+        // Set adjusted delta based on event data delta to fit min and max bounds
+        Vector2 adjustedDelta;
+        {
+            float magnitude = eventData.delta.magnitude;
+            if (magnitude < minForce)
+            {
+                // Set adjustedDelta to up in case it's zero, or in case it's juts very low, then set it to minimal value
+                if (Mathf.Approximately(magnitude, 0.0f))
+                {
+                    adjustedDelta = minForce * Vector2.up;
+                }
+                else
+                {
+                    adjustedDelta = minForce * eventData.delta.normalized;
+                }
+            }
+            else
+            {
+                // Set adjusted delta to max value in case it's too big, or just the normal value otherwise
+                if (eventData.delta.magnitude > maxForce)
+                {
+                    adjustedDelta = maxForce * eventData.delta.normalized;
+                }
+                else
+                {
+                    adjustedDelta = eventData.delta;
+                }
+            }
+        }
+
         // Calculate directional forces with offset
         const float OFFSET = 15;
-        Vector3 forceDirection1 = Quaternion.Euler(0, 0, OFFSET) * eventData.delta;
-        Vector3 forceDirection2 = Quaternion.Euler(0, 0, -OFFSET) * eventData.delta;
+        Vector3 forceDirection1 = Quaternion.Euler(0, 0, OFFSET) * adjustedDelta;
+        Vector3 forceDirection2 = Quaternion.Euler(0, 0, -OFFSET) * adjustedDelta;
 
         // Apply the forces with rotationMultiplier
         piece.Initialize(force: forceDirection1 * forceMultiplier, (transform.eulerAngles.z < 180.0f) ? 1.0f : -1.0f);
